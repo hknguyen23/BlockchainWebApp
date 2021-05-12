015b75767410ebd713b367e047d68fcd48eafcb0
Khanh Nguyen Ho <hokhanhnguyen23@gmail.com>
2021-05-12 19:26:20 +0700

    fix backend

--- blockchain-back/routes/index.js
+++ blockchain-back/routes/index.js
@@ -1,9 +1,3 @@
 const express = require('express');
 const router = express.Router();
-
-router.use('/', require('./home'));
-router.use('/users', require('./users'));
-router.use('/wallets', require('./wallets'));
-router.use('/transactions', require('./transactions'));
-
 module.exports = router;
